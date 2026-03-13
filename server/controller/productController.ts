f11bafb657c16147b2c3a32270bfe7f463d0e3d0
Siddu Mokhasi <siddu124123@gmail.com>
2026-03-13 12:55:51 +0530

    completed the toggle quantity and delete product feature

--- server/controller/productController.ts
+++ server/controller/productController.ts
@@ -27,12 +27,63 @@ export const getProductById = async (req: Request, res: Response) => {
         if (!id) {
             return res.json({ success: false, message: "Id not found" })
         }
-        const product = await Product.findById(id).populate("shopId","shopName location phone");
+        const product = await Product.findById(id).populate("shopId", "shopName location phone");
         if (!product) {
             return res.json({ success: false, message: "Product not found" })
         }
 
         return res.json({ success: true, product })
+    } catch (error: any) {
+        return res.json({ success: false, message: error.message })
+    }
+}
+
+// PATCH: /api/products/:id/quantity
+// update product quantity
+
+export const updateQuantity = async (req: Request, res: Response) => {
+    try {
+        const { quantity } = req.body;
+        const { id } = req.params;
+        if (quantity === undefined || quantity === null) {
+            return res.json({ success: false, message: "Quantity missing" })
+        }
+        if (!id) {
+            return res.json({ success: false, message: "Id missing" })
+        }
+        const product = await Product.findById(id);
+        if (!product) {
+            return res.json({ success: false, message: "Product not found" })
+        }
+
+        product.quantity = quantity;
+        await product.save();
+
+        return res.json({ success: true, message: "Quantity Updated!" })
+
+    } catch (error: any) {
+        return res.json({ success: false, message: error.message })
+    }
+}
+
+
+// DELETE: /api/products/:id
+// delete the product
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        if (!id) {
+            return res.json({ success: false, message: "Id not found" })
+        }
+
+        const product = await Product.findById(id);
+        if (!product) {
+            return res.json({ success: false, message: "Product not found" })
+        }
+
+        await Product.findByIdAndDelete(id);
+        return res.json({ success: true, message: "Product Deleted!" })
     } catch (error: any) {
         return res.json({ success: false, message: error.message })
     }
--- server/route/productRoure.ts
+++ server/route/productRoure.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { getAllProducts, getProductById } from "../controller/productController.js";
+import { deleteProduct, getAllProducts, getProductById, updateQuantity } from "../controller/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.get("/", getAllProducts);
 productRouter.get("/:id", getProductById);
+productRouter.patch("/:id/quantity", updateQuantity);
+productRouter.delete("/:id", deleteProduct);
 
 
 export default productRouter
